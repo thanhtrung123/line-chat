2a733738b8ab365897ed57ed0cfbbfef21f38a6f
Trung. Ngo Thanh <trung.nt@globaldesignit.vn>
2021-01-12 15:44:05 +0700

    ssss

--- index.php
+++ index.php
@@ -18,6 +18,24 @@ $app = new Slim\App($configs);
 /* ROUTES */
 $app->get('/', function ($request, $response) {
 	// init bot
+	// get request body and line signature header
+	$body 	   = file_get_contents('php://input');
+	$signature = $_SERVER['HTTP_X_LINE_SIGNATURE'];
+
+	// log body and signature
+	file_put_contents('php://stderr', 'Body: '.$body);
+
+	// is LINE_SIGNATURE exists in request header?
+	if (empty($signature)){
+		return $response->withStatus(400, 'Signature not set');
+	}
+
+	// is this request comes from LINE?
+	if($_ENV['PASS_SIGNATURE'] == false && ! SignatureValidator::validateSignature($body, $_ENV['CHANNEL_SECRET'], $signature)){
+		return $response->withStatus(400, 'Invalid signature');
+	}
+
+	// init bot
 	$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV['CHANNEL_ACCESS_TOKEN']);
 	$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV['CHANNEL_SECRET']]);
 	$data = json_decode($body, true);
